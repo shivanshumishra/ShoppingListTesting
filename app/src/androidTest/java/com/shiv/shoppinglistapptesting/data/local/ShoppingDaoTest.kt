cc120bfdc2125029f1ac024d8a7dc435d04f1c5b
shivanshumishra <shivanshufeb14@gmail.com>
2023-06-01 00:32:21 +0530

    dao tests added

--- app/src/androidTest/java/com/shiv/shoppinglistapptesting/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/shiv/shoppinglistapptesting/data/local/ShoppingDaoTest.kt
@@ -1,20 +1,31 @@
 package com.shiv.shoppinglistapptesting.data.local
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.shiv.shoppinglistapptesting.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class ShoppingDaoTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var database: ShoppingItemDatabase
     private lateinit var dao: ShoppingDao
+
+
 
     @Before
     fun setup(){
@@ -42,6 +53,24 @@ class ShoppingDaoTest {
         )
         dao.insertShoppingItem(shoppingItem)
 
-        val allShoppingItems = dao.observeAllShoppingItems()
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+        assertThat(allShoppingItems).contains(shoppingItem)
+    }
+
+    @Test
+    fun deleteShoppingItem() = runBlockingTest {
+        val shoppingItem = ShoppingItem(
+            "name",
+            1,
+            1f,
+            "url",
+            1
+        )
+        dao.insertShoppingItem(shoppingItem)
+        dao.deleteShoppingItem(shoppingItem)
+
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+
+        assertThat(allShoppingItems).doesNotContain(shoppingItem)
     }
 }
